99ebebd5fd48d5ec1bc632894309cc422bc46335
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2024-05-16 00:21:49 +0300

    service-registry and file-category service package added

---
+++ file-category/src/main/java/com/kadiraksoy/filecategory/FileCategoryApplication.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.filecategory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileCategoryApplication.class, args);
+	}
+
+}
---
+++ file-category/src/test/java/com/kadiraksoy/filecategory/FileCategoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.filecategory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileCategoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
