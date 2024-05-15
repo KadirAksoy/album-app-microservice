dcc94055c117e252fb37c209739f3ee9965c7a25
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2024-05-15 16:18:14 +0300

    docker-compose.yml fixed

--- user-service/src/main/java/com/kadiraksoy/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/kadiraksoy/userservice/UserServiceApplication.java
@@ -1,11 +1,9 @@
 package com.kadiraksoy.userservice;
 
-import com.kadiraksoy.userservice.model.User;
-import com.kadiraksoy.userservice.service.UserService;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class UserServiceApplication {
