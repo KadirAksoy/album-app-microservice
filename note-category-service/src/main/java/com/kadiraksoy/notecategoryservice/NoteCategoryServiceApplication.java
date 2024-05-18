880e352242cad975b030e18b68dd1e7719bf5c24
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2024-05-18 10:59:33 +0300

    note-category-service added

---
+++ note-category-service/src/main/java/com/kadiraksoy/notecategoryservice/NoteCategoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.notecategoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteCategoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteCategoryServiceApplication.class, args);
+	}
+
+}
